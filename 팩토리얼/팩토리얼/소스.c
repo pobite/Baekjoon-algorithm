1d066dd239dc88605d46c6678876640bd64ae093
pobite <67305866+pobite@users.noreply.github.com>
2022-02-25 20:24:18 +0900

    baekjoon: 10872
    백준 10872: 팩토리얼

---
+++ 팩토리얼/팩토리얼/소스.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int factorial(int n) {
+	
+	if (n > 1) {
+		return n * factorial(n - 1); 
+	}
+	else {
+		return 1;
+	}
+}
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+	printf("%d", factorial(n));
+	return 0;
+}
